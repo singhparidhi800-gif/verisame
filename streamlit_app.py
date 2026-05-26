542b13f8df2849f566a0b825d664edb69428f904
singhparidhi800-gif <singhparidhi800@gmail.com>
2026-05-26 05:35:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -773,7 +773,7 @@ C303,Category_Z,01/04/2024,200,MALE,another@test.in,9988776655"""
                 st.warning("⚠️ You have 1 Month plan active. To upgrade to 6 Months, please pay ₹1499.")
                 st.session_state.payment_done = False
 
-             if is_active and ((user_has_month and selected_is_month) or (user_has_half)):
+            if is_active and ((user_has_month and selected_is_month) or (user_has_half)):
                 st.success(f"✅ Download Unlocked till {st.session_state.pro_expiry}")
                 excel_buffer = BytesIO()
                 with pd.ExcelWriter(excel_buffer, engine='openpyxl') as writer:
